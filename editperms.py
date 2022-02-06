62eb875627618829044ad811ce30f389693844e4
damiens <72633854+damiens0@users.noreply.github.com>
2022-02-06 15:20:21 -0600

    Update editperms.py

--- editperms.py
+++ editperms.py
@@ -1,4 +1,5 @@
 import subprocess
 import os
-usr = subprocess.check_output('whoami')
-os.system("sudo chown {0} /usr/bin/pimarket".format(usr))
+usr = subprocess.check_output('whoami').decode('utf-8')
+usr_2 = usr.replace('\n', "")
+os.system("sudo chown {0} /usr/bin/pimarket".format(usr_2))
